19acc737de30c8d00748fb4f302df776d471e4f9
Juan Biancuzzo <jbiancuzzo@fi.uba.ar>
2021-09-24 22:34:01 -0300

    Creando una carpeta donde se encargue de el movimiento del jugador

--- prototipo-downward-platformer/Assets/Scripts/PlayerMovement/PlayerController.cs
+++ prototipo-downward-platformer/Assets/Scripts/PlayerMovement/PlayerController.cs
@@ -6,15 +6,10 @@ public class PlayerController : MonoBehaviour
 {
     public IInputProvider inputProvider = new InputProviderPlayer();
 
-    // Start is called before the first frame update
     void Start()
     {
         inputProvider.AgregarInputMiddleware(new InputSystem());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
